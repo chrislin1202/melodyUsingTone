01a52e2a61f54d5e8f772d31311fec01f48ead08
yue lin(Christine) <Christine@Christine-MacB.wireless.newschool.edu>
2015-12-08 22:14:18 -0500

    arduinoMelodyHW

---
+++ melodyUsingTone.ino
@@ -0,0 +1,110 @@
+/*
+  Melody
+
+ Plays a melody
+
+ circuit:
+ * 8-ohm speaker on digital pin 8
+
+ created 21 Jan 2010
+ modified 30 Aug 2011
+ by Tom Igoe
+
+This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/Tone
+
+ */
+#include "pitches.h"
+
+int output = 8;
+int button = 9;
+int buttonState;
+int lastButtonState = LOW;
+int state = HIGH;
+
+long time = 0;
+long debounce = 50;
+
+
+int melody[] = {
+  //  NOTE_C5, NOTE_G4, NOTE_G4, NOTE_A4, NOTE_G4, 0, NOTE_B4, NOTE_C4,
+  NOTE_E5, NOTE_E5, 0, NOTE_E5, 0, NOTE_C5, NOTE_E5, NOTE_G5, 0, 0, NOTE_G4, 0, 0,
+  NOTE_C5, 0, NOTE_G4, 0, NOTE_E4, 0, NOTE_A4, 0, NOTE_B4, 0, NOTE_AS4, NOTE_A4,
+  NOTE_G4, NOTE_E5, NOTE_G5, NOTE_A5, NOTE_F5, NOTE_G5, 0, NOTE_E5, 0, NOTE_C5, NOTE_D5, NOTE_B4, 0,
+  NOTE_C5, 0, NOTE_G4, 0, NOTE_E4, 0, NOTE_A4, 0, NOTE_B4, 0, NOTE_AS4, NOTE_A4,
+  NOTE_G4, NOTE_E5, NOTE_G5, NOTE_A5, NOTE_F5, NOTE_G5, 0, NOTE_E5, 0, NOTE_C5, NOTE_D5, NOTE_B4, 0,
+
+  0, NOTE_G5, NOTE_FS5, NOTE_F5, NOTE_DS5, 0, NOTE_E5, 0, NOTE_GS4, NOTE_A4, NOTE_C5, 0, NOTE_A4, NOTE_C5, NOTE_D5,
+  0, NOTE_G5, NOTE_FS5, NOTE_F5, NOTE_DS5, 0, NOTE_E5, 0, NOTE_C6, 0, NOTE_C6, NOTE_C6,
+  0, NOTE_G5, NOTE_FS5, NOTE_F5, NOTE_DS5, 0, NOTE_E5, 0, NOTE_GS4, NOTE_A4, NOTE_C5, 0, NOTE_A4, NOTE_C5, NOTE_D5,
+  0, NOTE_DS5, 0, NOTE_D5, 0, NOTE_C5, 0, 0,
+
+  0, NOTE_G5, NOTE_FS5, NOTE_F5, NOTE_DS5, 0, NOTE_E5, 0, NOTE_GS4, NOTE_A4, NOTE_C5, 0, NOTE_A4, NOTE_C5, NOTE_D5,
+  0, NOTE_G5, NOTE_FS5, NOTE_F5, NOTE_DS5, 0, NOTE_E5, 0, NOTE_C6, 0, NOTE_C6, NOTE_C6,
+  0, NOTE_G5, NOTE_FS5, NOTE_F5, NOTE_DS5, 0, NOTE_E5, 0, NOTE_GS4, NOTE_A4, NOTE_C5, 0, NOTE_A4, NOTE_C5, NOTE_D5,
+  0, NOTE_DS5, 0, NOTE_D5, 0, NOTE_C5, 0, 0,
+
+  NOTE_C5, NOTE_C5, NOTE_C5, 0, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_C5, NOTE_A4, NOTE_G4, 0,
+  NOTE_C5, NOTE_C5, NOTE_C5, 0, NOTE_C5, NOTE_D5, NOTE_E5, 0, 0,
+  NOTE_C5, NOTE_C5, NOTE_C5, 0, NOTE_C5, NOTE_D5, NOTE_E5, NOTE_C5, NOTE_A4, NOTE_G4, 0,
+  NOTE_E5, NOTE_E5, NOTE_E5, 0, NOTE_C5, NOTE_E5, NOTE_G5, 0, NOTE_G4, 0,
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  8, 8, 8, 8, 8, 8, 4, 8, 8, 4, 8, 8, 4,
+  6, 4, 6, 4, 6, 4, 8, 8, 8, 8, 8, 4,
+  8, 8, 8, 4, 8, 8, 8, 8, 8, 8, 8, 8, 4,
+  6, 4, 6, 4, 6, 4, 8, 8, 8, 8, 8, 4,
+  8, 8, 8, 4, 8, 8, 8, 8, 8, 8, 8, 8, 4,
+  //60
+
+  4, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+  4, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4,
+  4, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+  4, 6, 4, 6, 4, 4, 4, 2,
+  //49
+
+  4, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+  4, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4,
+  4, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+  4, 6, 4, 6, 4, 4, 4, 2,
+  //49
+
+  //158
+
+  8, 4, 8, 8, 8, 4, 8, 4, 8, 4, 4,
+  8, 4, 8, 8, 8, 4, 4, 4, 2,
+  8, 4, 8, 8, 8, 4, 8, 4, 8, 4, 4,
+  8, 4, 8, 8, 8, 4, 4, 4, 4, 4,
+  //41
+
+  6, 4, 6, 4, 6, 4, 8, 8, 8, 8, 8, 4,
+  8, 8, 8, 4, 8, 8, 8, 8, 8, 8, 8, 8, 4,
+  6, 4, 6, 4, 6, 4, 8, 8, 8, 8, 8, 4,
+  8, 8, 8, 4, 8, 8, 8, 8, 8, 8, 8, 8, 4,
+  //50
+
+};
+
+void setup() {
+  pinMode(button, INPUT);
+}
+
+void loop() {
+  
+  for (int thisNote = 0; thisNote < 249; thisNote++) {
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(output, melody[thisNote], noteDuration);
+    // to distinguish the notes, set a minimum time between them.
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(output);
+  }
+}
